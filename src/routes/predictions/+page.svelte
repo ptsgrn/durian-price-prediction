c11d48c391f97c85c65e36dddd5fe614eca542f0
PhubestSrikooon <forbf100@gmail.com>
2023-01-02 22:05:52 +0700

    add : stupid and unreliable post system

--- src/routes/predictions/+page.svelte
+++ src/routes/predictions/+page.svelte
@@ -2,6 +2,7 @@
 	import Heading from '$lib/components/Heading.svelte';
 	const formatMonth = Intl.DateTimeFormat('th-TH', { month: 'long' }).format;
 	const formatYear = Intl.DateTimeFormat('th-TH', { year: 'numeric' }).format;
+
 	const priceData = [
 		{
 			monthYear: '2021-01',
@@ -51,65 +52,90 @@
 			monthYear: '2021-12',
 			price: 123
 		},
-    {
-      monthYear: '2022-01',
-      price: 150
-    },
-    {
-      monthYear: '2022-02',
-      price: 150
-    },
-    {
-      monthYear: '2022-03',
-      price: 150
-    },
-    {
-      monthYear: '2022-04',
-      price: 150
-    },
-    {
-      monthYear: '2022-05',
-      price: 150
-    },
-    {
-      monthYear: '2022-06',
-      price: 150
-    },
-    {
-      monthYear: '2022-07',
-      price: 150
-    },
-    {
-      monthYear: '2022-08',
-      price: 150
-    },
-    {
-      monthYear: '2022-09',
-      price: 150
-    },
-    {
-      monthYear: '2022-10',
-      price: 150
-    },
-    {
-      monthYear: '2022-11',
-      price: 150
-    },
-    {
-      monthYear: '2022-12',
-      price: 150
-    }
+		{
+			monthYear: '2022-01',
+			price: 150
+		},
+		{
+			monthYear: '2022-02',
+			price: 150
+		},
+		{
+			monthYear: '2022-03',
+			price: 150
+		},
+		{
+			monthYear: '2022-04',
+			price: 150
+		},
+		{
+			monthYear: '2023-05',
+			price: 150
+		},
+		{
+			monthYear: '2023-06',
+			price: 150
+		},
+		{
+			monthYear: '2023-07',
+			price: 150
+		},
+		{
+			monthYear: '2023-08',
+			price: 150
+		},
+		{
+			monthYear: '2023-09',
+			price: 150
+		},
+		{
+			monthYear: '2023-10',
+			price: 150
+		},
+		{
+			monthYear: '2023-11',
+			price: 150
+		},
+		{
+			monthYear: '2023-12',
+			price: 150
+		}
+		
 	];
+	/**
+	 * @type {string | null}
+	 */
+	let previousYear = null;
+	let currentYear = null;
 </script>
 
 <Heading tag="h1">พยากรณ์ราคา</Heading>
 <div class="grid grid-flow-row border-2 border-blue-700 p-5">
 	<div>
 		<Heading tag="h2">พยากรณ์ราคาทุเรียนหมอนทอง</Heading>
-		<span>เดือน {currentMonth} ปี {currentYear}</span>
-		<span>159</span>
-		<span>บาท/กิโลกรัม</span>
-		<div />
+		<span
+			>เดือน
+			<select name="Month" id="0">
+				{#each priceData as { monthYear, price }, i (monthYear)}
+					{#if formatYear(new Date()) === formatYear(new Date(monthYear))}
+						<option value={i}>{formatMonth(new Date(monthYear))}</option>
+					{/if}
+				{/each}
+			</select>
+			ปี
+			<select name="Years" id="1">
+				{#each priceData as { monthYear, price }, i (monthYear)}
+					{#if i != 0}
+						{previousYear = formatYear(new Date(priceData[i - 1].monthYear))}
+					{/if}
+					{currentYear = formatYear(new Date(priceData[i].monthYear))}
+					{#if previousYear != currentYear}
+						<option value={i}>{currentYear}</option>
+					{/if}
+				{/each}
+			</select></span
+		>
+		<div class="text-center"><span class="text-[50vh]">159</span><span>บาท/กิโลกรัม</span></div>
 	</div>
 	<div />
 </div>
