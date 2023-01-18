e696ccd9bbfba272ba25a9305683f5fff609af5f
Patsagorn <49602385+ptsgrn@users.noreply.github.com>
2023-01-18 14:21:49 +0700

    add version number

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.postcss';
 	import { Footer } from 'flowbite-svelte';
+	import { version } from '$app/environment';
 	import FooterCopyright from '$lib/components/FooterCopyright.svelte';
 	import Navigation from '$lib/components/Navigation.svelte';
 </script>
@@ -10,12 +11,13 @@
 	<main id="main" class="px-5 dark:bg-slate-800 md:w-[47rem] m-auto  duration-200">
 		<slot />
 	</main>
-	<Footer class="mx-auto align-middle justify-center items-center">
+	<Footer class="mx-auto align-middle justify-center items-center flex-col">
 		<FooterCopyright
 			class="mx-auto w-full"
 			href="https://ptsgrn.github.io"
 			by="Patsagorn Y."
 			copyrightMessage="Where applicable, this site was commissioned and is protected by the owner's copyright as a component of that project."
 		/>
+		<div class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Version: {Number(version).toString(16)}</div>
 	</Footer>
 </div>
